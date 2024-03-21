5c379ff194cf6bb21bc8761b28e502e523b21d0a
Quang Truong <qtruongngoc95@gmail.com>
2024-03-22 06:21:26 +0700

    feat: init api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <>
+    <Header />
       <Outlet />
     </>
   );
