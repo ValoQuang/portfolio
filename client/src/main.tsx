ff0fdac15d221ecfd36f5d31894c936a978f8308
Quang Truong <qtruongngoc95@gmail.com>
2024-03-22 15:14:21 +0700

    feat: redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { store } from "./redux/store";
+import { Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    <Provider store={store}>
+      <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    </Provider>
   </React.StrictMode>
 );
